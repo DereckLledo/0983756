a90108b9176e4a9b39a7935fa1d381dea921a5bd
Dereck Lledo <derecklledo@gmail.com>
2018-09-09 13:54:33 -0400

    atelier02

--- app/src/main/java/ca/cours5b5/derecklledo/Parametres.java
+++ app/src/main/java/ca/cours5b5/derecklledo/Parametres.java
@@ -10,6 +10,6 @@ public class Parametres extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_parametres);
-        Log.d("MonEtiquette","Bonjour!");
+        Log.d("MonEtiquette",this.getResources().getString(R.string.bonjour) + " " + this.getResources().getString(R.string.orientation));
     }
 }
