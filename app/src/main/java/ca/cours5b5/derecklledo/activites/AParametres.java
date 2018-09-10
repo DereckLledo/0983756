f3d4a0c3ee0c1ce56dbb8813ff84b60017e8a296
Dereck Lledo <derecklledo@gmail.com>
2018-09-10 14:52:17 -0400

    REMISE: atelier#3

--- app/src/main/java/ca/cours5b5/derecklledo/activites/AParametres.java
+++ app/src/main/java/ca/cours5b5/derecklledo/activites/AParametres.java
@@ -1,10 +1,12 @@
-package ca.cours5b5.derecklledo;
+package ca.cours5b5.derecklledo.activites;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
-public class Parametres extends AppCompatActivity {
+import ca.cours5b5.derecklledo.R;
+
+public class AParametres extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
---
+++ app/src/main/java/ca/cours5b5/derecklledo/global/GConstantes.java
@@ -0,0 +1,17 @@
+package ca.cours5b5.derecklledo.global;
+
+public class GConstantes {
+
+    public static final int MIN_HAUTEUR = 4;
+    public static final int MAX_HAUTEUR = 10;
+    public static final int DEFAUT_HAUTEUR = 6;
+
+    public static final int MIN_LARGEUR = 4;
+    public static final int MAX_LARGEUR = 10;
+    public static final int DEFAUT_LARGEUR = 7;
+
+    public static final int MIN_GAGNER = 3;
+    public static final int MAX_GAGNER = 4;
+    public static final int DEFAUT_GAGNER = 4;
+
+}
